ecd73ebdf870c5953b21f55bdcec72e6ee7cd90c
suglow006 <suglow006@gmail.com>
2022-11-03 04:03:29 +0000

    feat(Charter3): finish subscripe newsltter

--- src/startup.rs
+++ src/startup.rs
@@ -1,26 +1,17 @@
+use crate::routes::health_check;
+use crate::routes::subscribe;
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-#[derive(serde::Deserialize)]
-struct FormData {
-    email : String,
-    name : String
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
